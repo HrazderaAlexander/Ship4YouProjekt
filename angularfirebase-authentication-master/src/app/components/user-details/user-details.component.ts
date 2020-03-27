4c0771b431381ea998efa114917aa78351699571
Hrazdi <alexander.hrazdera@gmail.com>
2020-03-27 08:24:25 +0100

    Change File

---
+++ angularfirebase-authentication-master/src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { AuthService } from "../../shared/services/auth.service";
+import { Router } from "@angular/router";
+
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    public ngZone: NgZone
+  ) { }
+
+  ngOnInit() { }
+
+}
